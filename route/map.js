163833b36b6fcd79c9f1246255f55f3dc089dc00
Lorena Smienk <50989058+lsmienk@users.noreply.github.com>
2020-07-31 14:24:31 +0200

    feat: rainbow road (#40)

--- route/map.js
+++ route/map.js
@@ -79,6 +79,7 @@ const drawPolyline = coordinates => {
 
   map.addSource('polyline-source', {
     type: 'geojson',
+    lineMetrics: true,
     data: geojson,
   });
 
@@ -92,8 +93,27 @@ const drawPolyline = coordinates => {
       'line-cap': 'round',
     },
     paint: {
-      'line-color': '#0078FF',
-      'line-width': 3,
+      'line-color': 'red',
+      'line-width': 8,
+      // 'line-gradient' must be specified using an expression
+      // with the special 'line-progress' property
+      'line-gradient': [
+        'interpolate',
+        ['linear'],
+        ['line-progress'],
+        0,
+        'blue',
+        0.1,
+        'royalblue',
+        0.3,
+        'cyan',
+        0.5,
+        'lime',
+        0.7,
+        'yellow',
+        1,
+        'red',
+      ],
     },
   });
 };
