8a73792f7ed1039ed5de5b19721e85494fd5d83b
Khrystyna Skvarok <christina.skvarok@gmail.com>
2020-12-07 10:57:48 +0100

    feat: added 'Drive with Google' button to route example (#62)
    feat: added 'Drive with Google' button to route example

--- route/index.js
+++ route/index.js
@@ -102,6 +102,12 @@ const drawRoutePolyline = data => {
   const reversed = decodedData.map(item => item.reverse());
 
   drawRoute(reversed, data.legs);
+
+  const driveBtn = document.getElementById('drive');
+  driveBtn.removeAttribute('disabled');
+  driveBtn.addEventListener('click', () => {
+    window.open(getGoogleMapDirectionsURL(data.legs));
+  });
 };
 
 /**
@@ -138,4 +144,37 @@ const displayRouteData = data => {
 
   // the total amount of CO2 which were used with a petrol vehicle
   document.getElementById('co2').innerHTML = data.saving?.co2 ? `${data.saving.co2 / 1000} Kg` : 'Unknown';
+};
+
+/**
+ * Create a Google Map Directions URL.
+ * See documentation here https://developers.google.com/maps/documentation/urls/get-started#directions-action.
+ *
+ * @param legs Route legs (origin, destination, waypoints and stations)
+ * @returns {string} Google Map Directions URL
+ */
+const getGoogleMapDirectionsURL = legs => {
+  if (legs.length === 0) return;
+
+  let googleDirURL = `https://google.com/maps/dir/?api=1`;
+  const origin = legs[0].origin?.geometry?.coordinates;
+  const destination = legs[legs.length - 1].destination?.geometry?.coordinates;
+
+  // coordinates are an array with longitude as first value and latitude as the second one
+  // we have to reverse it as Google Maps accept latitude first
+  googleDirURL += `&origin=${origin?.reverse()?.join(',')}&destination=${destination?.reverse()?.join(',')}`;
+
+  if (legs.length > 2) {
+    googleDirURL += `&waypoints=`;
+    legs.map((leg, index) => {
+      // add charging stations and waypoints
+      if (index !== legs.length - 1) {
+        googleDirURL += `${leg.destination?.geometry?.coordinates?.reverse()?.join(',')}|`;
+      }
+    });
+  }
+
+  googleDirURL += `&dir_action=navigate&travelmode=driving`;
+
+  return encodeURI(googleDirURL);
 };
