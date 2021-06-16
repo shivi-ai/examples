6b93bc018745d00e52a61142d0cababc0fab4294
Wouter van de Kamp <wouter@thisisnota.studio>
2021-06-16 16:59:49 +0200

    fix: UI for the slider has been updated

--- elevation-plot/index.js
+++ elevation-plot/index.js
@@ -26,12 +26,19 @@ const drawRoutePolyline = (id, data) => {
 
   drawRoute(id, reversed, data?.legs);
   renderRouteHeader(data);
-
   renderRouteDetails(data.pathPlot[0]);
 
-  const plotSlider = document.getElementById('myRange');
+  const plotSliderInput = document.getElementById('slider-input');
+  const plotSliderOutput = document.getElementById('slider-output');
 
-  plotSlider.addEventListener('input', () => {
-    renderRouteDetails(data.pathPlot[plotSlider.value]);
+  plotSliderInput.addEventListener('input', () => {
+    renderRouteDetails(data.pathPlot[plotSliderInput.value]);
+
+    const val = plotSliderInput.value;
+    const min = plotSliderInput.min ? plotSliderInput.min : 0;
+    const max = plotSliderInput.max ? plotSliderInput.max : 100;
+    const newVal = Number(((val - min) * 100) / (max - min));
+
+    plotSliderOutput.style.left = `calc(${newVal}% + (${11 - newVal * 0.25}px))`;
   });
 };
